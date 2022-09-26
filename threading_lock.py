a8c4a68574bc83bf082fe380907677331a6ebc7a
dennislee22 <35444414+dennislee22@users.noreply.github.com>
2022-09-26 12:32:51 +0800

    Create threading_lock.py

---
+++ threading_lock.py
@@ -0,0 +1,49 @@
+import psutil
+import threading
+import os
+import time
+
+global_lock = threading.Lock()
+
+open('zzz', 'w').close()    
+
+y = 0
+def write_to_file():
+    with open("asd",'r') as fp:
+      x = len(fp.readlines())
+    while global_lock.locked():
+        continue
+    global_lock.acquire()
+    with open("zzz", "a+") as file:
+      file.write("current threads:" + str(threading.current_thread()))
+      file.write("\n")
+      file.write("current pid:" + str(os.getpid()))
+      file.write("\n")
+      file.write("total threads:" + str(psutil.Process().num_threads()))
+      file.write("\n")
+      file.write("CPU number:" + str(psutil.Process().cpu_num()))
+      file.write("\n")
+      with open("asd",'r') as ok:
+        global y
+        y -= 1 
+        content = ok.readlines()
+        file.write(content[y])
+        print(y)
+      file.write("\n")
+      file.close()
+
+    global_lock.release()
+
+threads = []
+
+start_time = time.perf_counter()
+
+for i in range(1, 21):
+    t = threading.Thread(target=write_to_file)
+    threads.append(t)
+    t.start()
+[thread.join() for thread in threads]
+
+end_time = time.perf_counter()  
+execution_time = end_time - start_time  
+print(f"\nJob Starts: {start_time}\nJob Ends: {end_time}\nTotals Execution Time:{execution_time:0.2f} seconds.")
