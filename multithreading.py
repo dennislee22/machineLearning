8c49d146cc9b6cb7babeedfcd813d3ee34d98392
dennislee22 <35444414+dennislee22@users.noreply.github.com>
2022-10-06 15:31:11 +0800

    Create multithreading.py

---
+++ multithreading.py
@@ -0,0 +1,42 @@
+import psutil
+import threading
+import os
+import time
+
+open('output', 'w').close()    
+with open("input",'r') as fp:
+  x = len(fp.readlines())
+fp.close()
+y = 0
+
+def write_to_file():
+    with open("output", "a+") as file:
+      file.write("current thread:" + str(threading.current_thread()))
+      file.write("\n")
+      file.write("current pid:" + str(os.getpid()))
+      file.write("\n")
+      file.write("total threads:" + str(psutil.Process().num_threads()))
+      file.write("\n")
+      file.write("CPU number:" + str(psutil.Process().cpu_num()))
+      file.write("\n")
+      #time.sleep(1)
+      with open("input",'r') as ok:
+        global y
+        content = ok.readlines()
+        file.write("write " + content[y])
+        #print(y)
+        y += 1
+      file.write("\n")
+      file.close()
+
+#threads = []
+
+if __name__ == "__main__":
+  start_time = time.perf_counter()
+  for i in range(y, x):
+    t = threading.Thread(target=write_to_file)
+    t.start()
+    t.join()
+  end_time = time.perf_counter()  
+  execution_time = end_time - start_time  
+  print(f"\nJob Starts: {start_time}\nJob Ends: {end_time}\nTotals Execution Time:{execution_time:0.2f} seconds.")
