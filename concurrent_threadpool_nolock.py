c991d0a73d6d5da347fe893f8fe36bd7b4fff5b1
dennislee22 <35444414+dennislee22@users.noreply.github.com>
2022-10-06 17:27:24 +0800

    Update concurrent_threadpool_nolock.py

--- concurrent_threadpool_nolock.py
+++ concurrent_threadpool_nolock.py
@@ -12,7 +12,7 @@ y = 0
 
 def write_to_file():
     with open("output", "a+") as file:
-      file.write("current threads:" + str(threading.current_thread()))
+      file.write("current thread:" + str(threading.current_thread()))
       file.write("\n")
       file.write("current pid:" + str(os.getpid()))
       file.write("\n")
@@ -20,17 +20,19 @@ def write_to_file():
       file.write("\n")
       file.write("CPU number:" + str(psutil.Process().cpu_num()))
       file.write("\n")
-      #time.sleep(1)
       with open("input",'r') as ok:
         global y
         content = ok.readlines()
-        file.write("write " + content[y])
-        print(y)
+        file.write(content[y])
+        #print(y)
         y += 1
       file.write("\n")
       file.close()
 
 if __name__ == "__main__":
   start_time = time.perf_counter()
-  with concurrent.futures.ThreadPoolExecutor(max_workers=x) as executor:
+  with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
     futures = [executor.submit(write_to_file) for i in range(x)]
+  end_time = time.perf_counter()  
+  execution_time = end_time - start_time  
+  print(f"\nJob Starts: {start_time}\nJob Ends: {end_time}\nTotals Execution Time:{execution_time:0.2f} seconds.")
